2e99c35222c3a6bfc7cd75e57e2a0da5a88a0a62
Alex Cherubin <agcpbg@yahoo.com>
2017-04-18 13:45:08 -0500

    feat(login) working w/o auth

--- db/index.js
+++ db/index.js
@@ -38,7 +38,7 @@ db.didSync = db.createAndSync()
 
 // sync the db, creating it if necessary
 // function createAndSync(force=app.isTesting, retries=0, maxRetries=5) {
-function createAndSync(force=true, retries=0, maxRetries=5) {
+function createAndSync(force=false, retries=0, maxRetries=5) {
   return db.sync({force})
     .then(() => debug(`Synced models to db ${url}`))
     .catch(fail => {
